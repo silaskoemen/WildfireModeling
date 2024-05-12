dbcaa289223729c1a63b96b2e2ab7f3769494c78
silaskoemen <kpsilas@gmail.com>
2024-05-13 00:57:38 +0100

    Added highest and lowest map plots

--- analyses/exploration.py
+++ analyses/exploration.py
@@ -8,9 +8,9 @@ import seaborn as sns
 df = pd.read_csv(f"{os.path.join( os.path.dirname( __file__ ), '..' )}/data/derived/data.csv")
 colors = ("#FC8A17", "#F73718")
 
-# Create heatmap with correlation values
+# Create heatmap with correlation values, similar to: https://medium.com/@szabo.bibor/how-to-create-a-seaborn-correlation-heatmap-in-python-834c0686b88e
 plt.figure(figsize=(14, 7))
-# define the mask to set the values in the upper triangle to True
+# mask for upper triangle
 mask = np.triu(np.ones_like(df.corr(), dtype=np.bool))
 heatmap = sns.heatmap(df.corr(), mask=mask, vmin=-1, vmax=1, annot=True, cmap='coolwarm')
 plt.savefig(f"{os.path.join(os.path.dirname( __file__ ), '..' )}/outputs/corr_heatmap.png")
@@ -23,12 +23,24 @@ for n in df.columns[:-1]: # without last to drop area
     plt.xlabel(n)
     plt.savefig(f"{os.path.join(os.path.dirname( __file__ ), '..' )}/outputs/scatter_area_{n}.png")
 
+# Scatterplots all in one image
+fig, ax = plt.subplots(3, 4, figsize=(13, 8))
+ax = ax.flatten()
+for i, n in enumerate(df.columns[:-1]): # without last to drop area
+    ax[i].scatter(df[n], df.area, c=colors[0], label = n)
+    ax[i].set_ylabel("log Area")
+    ax[i].set_xlabel(n)
+    ax[i].legend(loc="upper right")
+    ax[i].grid(alpha=.2)
+plt.tight_layout()
+plt.savefig(f"{os.path.join(os.path.dirname( __file__ ), '..' )}/outputs/all_scatters.png")
+plt.show()
+
 # Area/Incidents over map
 image_path = f"{os.path.join(os.path.dirname( __file__ ), '..' )}/data/raw/montesinho_map.png"
 image = plt.imread(image_path)
 
 fig, ax = plt.subplots()
-
 ax.imshow(image, aspect='auto') # 'auto' adjusts the aspect ratio to match the image
 
 for index, row in df.iterrows():
