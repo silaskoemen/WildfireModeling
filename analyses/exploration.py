ec5345c56e1dee962cc18fe11083a34548df457c
silaskoemen <kpsilas@gmail.com>
2024-05-12 23:12:05 +0100

    Added scatter plot of outcome against all variables in 1 plot and correlation heatmap

--- analyses/exploration.py
+++ analyses/exploration.py
@@ -3,20 +3,18 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+import seaborn as sns
 
 df = pd.read_csv(f"{os.path.join( os.path.dirname( __file__ ), '..' )}/data/derived/data.csv")
-colors = ("#FC8A17", "#F73718") # prob after have all put in src as list
+colors = ("#FC8A17", "#F73718")
 
-## Create covariance matrix for all features, could use for sampling
-# from sklearn.preprocessing import MinMaxScaler
-# norm_df = MinMaxScaler().fit_transform(df)
-# norm_df = pd.DataFrame(norm_df, columns = df.columns)
-# cov = np.cov(norm_df.drop("area", axis = 1), rowvar=False)
-# cov_df = pd.DataFrame(cov, columns=df.columns[:-1])
-# cov_df.index = df.columns[:-1]
-# https://www.statsmodels.org/stable/generated/statsmodels.discrete.count_model.ZeroInflatedPoisson.html
-# could model as zero inflated Poisson from rest
-# could then generate more from there from data distr.
+# Create heatmap with correlation values
+plt.figure(figsize=(14, 7))
+# define the mask to set the values in the upper triangle to True
+mask = np.triu(np.ones_like(df.corr(), dtype=np.bool))
+heatmap = sns.heatmap(df.corr(), mask=mask, vmin=-1, vmax=1, annot=True, cmap='coolwarm')
+plt.savefig(f"{os.path.join(os.path.dirname( __file__ ), '..' )}/outputs/corr_heatmap.png")
+plt.show()
 
 # Scatterplots against area for some early insights
 for n in df.columns[:-1]: # without last to drop area
@@ -31,10 +29,8 @@ image = plt.imread(image_path)
 
 fig, ax = plt.subplots()
 
-# Display the image with the correct aspect ratio
 ax.imshow(image, aspect='auto') # 'auto' adjusts the aspect ratio to match the image
 
-# Plot each point, scaling the size by the area value
 for index, row in df.iterrows():
     # Normalize the area to a suitable size for the plot
     size = row['area']*1.5
@@ -43,9 +39,6 @@ for index, row in df.iterrows():
     else:
         ax.scatter((row['X']-1)*84.5*1.15+42.25+np.random.normal(0, 8), (row['Y']-1)*51.9*1.13+25.95+np.random.normal(0, 6), s=size, color=colors[1], alpha=0.6)
 
-# Hide the axis
 ax.axis('off')
-
-# Show the plot
 plt.savefig(f"{os.path.join(os.path.dirname( __file__ ), '..' )}/outputs/map_fires.png")
 plt.show()
